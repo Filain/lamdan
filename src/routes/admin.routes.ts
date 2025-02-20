199d8548d0275dd5526b6f752c2fc026b1f6c27c
Filain <ratecurrent@gmail.com>
2025-02-20 16:26:09 +0200

    bannedMiddleware err

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { authMiddleware } from '../middleware/auth.middleware';
 import { adminController } from '../controllers/admin.controller';
+import { roleMiddleware } from '../middleware/role.middleware';
 
 const router = express.Router();
 
@@ -9,21 +10,25 @@ router.get('/', authMiddleware.checkAccessToken, adminController.getAll);
 router.get(
     '/statistics',
     authMiddleware.checkAccessToken,
+    roleMiddleware.isAdmin,
     adminController.getStatistics,
 );
 router.patch(
     '/ban/:userId',
     authMiddleware.checkAccessToken,
+    roleMiddleware.isAdmin,
     adminController.banUser,
 );
 router.patch(
     '/unban/:userId',
     authMiddleware.checkAccessToken,
+    roleMiddleware.isAdmin,
     adminController.unbanUser,
 );
 router.post(
     '/get-active/:userId',
     authMiddleware.checkAccessToken,
+    roleMiddleware.isAdmin,
     adminController.getActivationToken,
 );
 router.post('/activate/:token', adminController.activate);
